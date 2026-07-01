70492aa6d3b8b1b71811a92614b0298ed08a6239
jeongduckhui <40081877+jeongduckhui@users.noreply.github.com>
2026-07-01 20:03:16 +0900

    Add AG Grid example home page

---
+++ src/ag-grid-examples/pages/AgGridExampleHomePage.jsx
@@ -0,0 +1,11 @@
+import AgGridExampleLayout from "../components/AgGridExampleLayout";
+
+export default function AgGridExampleHomePage() {
+  return (
+    <AgGridExampleLayout title="AG Grid Examples" description="Example pages for AG Grid practice.">
+      <div>
+        Add a new example to agGridExampleConfig.jsx to show it in the menu and routes.
+      </div>
+    </AgGridExampleLayout>
+  );
+}
